57abb311cce92f68de76912bf0a11c3304b6efd6
martikan <ric.martikan@gmail.com>
2022-01-07 02:25:30 +0100

    Add trusted kafka package type

--- src/main/java/org/martikan/mastore/accountmanagementapi/config/KafkaConsumerConfig.java
+++ src/main/java/org/martikan/mastore/accountmanagementapi/config/KafkaConsumerConfig.java
@@ -29,11 +29,15 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
 
+        var deserializer = new JsonDeserializer<>();
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("org.martikan.mastore.userapi.dto.user.UserDTO");
+
         var configProps = new HashMap<String, Object>();
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
 
         return new DefaultKafkaConsumerFactory<>(configProps);
     }
